0cd6523957719ce9b11191343e888cecbced7b9f
Cristian Gotchev <christumer@gmail.com>
2025-03-25 22:35:03 -0600

    Update hough.py

--- hough.py
+++ hough.py
@@ -6,6 +6,8 @@ import sys
 import numpy as np
 from typing import NamedTuple
 
+# definición de tipo:
+# type LineLike = np.ndarray[tuple[int, int, int, int], np.dtype[np.int_]] # para python 3.12+
 LineLike = np.ndarray[tuple[int, int, int, int], np.dtype[np.int_]]
 
 colors = [
@@ -18,10 +20,19 @@ colors = [
 ]
 
 def out_name(name: str) -> str:
+    """
+    Regresa un string con el nombre del archivo inicial seguido de -out y la extensión.
+    Ejemplo: imagen1.jpg -> imagen1-out.jpg
+    """
+
     i = name.rfind('.')
     return name[:i] + '-out' + name[i:]
 
 class HoughParams(NamedTuple):
+    """
+    Empaqueta los parámetros del algoritmo de Hough.
+    """
+
     rho: float
     theta: float
     threshold: int
@@ -29,11 +40,23 @@ class HoughParams(NamedTuple):
     maxLineGap: float
 
 class LineDetector:
+    """
+    Contiene los métodos necesarios para detectar líneas en una imagen.
+    """
+
     def __init__(self):
         pass
 
-    def lines_too_close(self, a: LineLike, b: LineLike, threshold: float):
+    def __lines_too_close(self, a: LineLike, b: LineLike, threshold: float) -> bool:
+        """
+        Revisa si dos líneas están muy cerca.
+        Este método se usa en conjunto con LineDetector.__cleanup().
+        """
+
         def dist(x1: int, y1: int, x2: int, y2: int) -> float:
+            """
+            Distancia euclidiana entre puntos (x1, y1) y (x2, y2).
+            """
             return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 
         ax1, ay1, ax2, ay2 = a
@@ -49,7 +72,35 @@ class LineDetector:
             )
         )
 
-    def cleanup(self, lines: MatLike):
+    def __cleanup(self, lines: MatLike) -> MatLike:
+        """
+        Revisa todos los pares de líneas. Si hay dos muy cerca, elimina una del par.
+        Esto se hace con el propósito de mantener la imagen final limpia:
+
+        Si la línea original se ve así:
+        ```
+        . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . .
+        . . . # # # # # # # # # . . .
+        . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . .
+        ```
+
+        ```
+        Los contornos se forman así:
+        . . . . . . . . . . . . . . .
+        . . + - - - - - - - - - + . .
+        . . | . . . . . . . . . | . .
+        . . + - - - - - - - - - + . .
+        . . . . . . . . . . . . . . .
+        ```
+        De manera que por una línea en la imagen original, se tienen cuatro líneas en el contorno.
+        Las líneas más cortas son descartadas por el argumento `minLineLength`, pero que para
+        las largas hay que realizar un procesamiento adicional para eliminar una de estas.
+
+        Ese procesamiento adicional se realiza en este método.
+        """
+
         while True:
             change = False
 
@@ -57,7 +108,7 @@ class LineDetector:
                 for j in range(len(lines)):
                     if i == j: continue
 
-                    if self.lines_too_close(lines[i][0], lines[j][0], 15):
+                    if self.__lines_too_close(lines[i][0], lines[j][0], 15):
                         lines = np.delete(lines, j, 0)
                         change = True
                         break
@@ -75,6 +126,10 @@ class LineDetector:
         show_image: bool = True,
         save_image: bool = False,
     ):
+        """
+        Detecta las líneas.
+        """
+
         out = source.copy()
         gray = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
         edges = cv2.Canny(gray, 70, 60)
@@ -88,7 +143,7 @@ class LineDetector:
             minLineLength=minLineLength,
             maxLineGap=maxLineGap,
         )
-        lines = self.cleanup(lines)
+        lines = self.__cleanup(lines)
 
         i = 0
         for line in lines:
@@ -102,6 +157,7 @@ class LineDetector:
 
         if save_image:
             cv2.imwrite(out_name(sys.argv[1]), out)
+
 
 if __name__ == '__main__':
     ld = LineDetector()
