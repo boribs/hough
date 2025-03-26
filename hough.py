1e2886498c2e5faf1b92402688a978760d9a929e
Cristian Gotchev <christumer@gmail.com>
2025-03-25 21:59:37 -0600

    Update hough.py

--- hough.py
+++ hough.py
@@ -1,6 +1,7 @@
 import cv2
 import sys
 import numpy as np
+from typing import NamedTuple
 
 colors = [
     (0, 0, 255),
@@ -11,74 +12,94 @@ colors = [
     (0, 255, 255),
 ]
 
-def deseado(filename):
-    i = filename.rfind('.')
-    return filename[:i] + '-deseado' + filename[i:]
+def out(name):
+    i = name.rfind('.')
+    return name[:i] + '-out' + name[i:]
 
-def lines_too_close(a, b, threshold):
-    def dist(x1, y1, x2, y2):
-        d = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-        return d
+class HoughParams(NamedTuple):
+    rho: float
+    theta: float
+    threshold: float
+    minLineLength: float
+    maxLineGap: float
 
-    ax1, ay1, ax2, ay2 = a
-    bx1, by1, bx2, by2 = b
+class LineDetector:
+    def __init__(self):
+        pass
 
-    return (
-        (
-            dist(ax1, ay1, bx1, by1) < threshold and
-            dist(ax2, ay2, bx2, by2) < threshold
-        ) or (
-            dist(ax1, ay1, bx2, by2) < threshold and
-            dist(ax2, ay2, bx1, by1) < threshold
+    def lines_too_close(self, a, b, threshold):
+        def dist(x1, y1, x2, y2):
+            return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+        ax1, ay1, ax2, ay2 = a
+        bx1, by1, bx2, by2 = b
+
+        return (
+            (
+                dist(ax1, ay1, bx1, by1) < threshold and
+                dist(ax2, ay2, bx2, by2) < threshold
+            ) or (
+                dist(ax1, ay1, bx2, by2) < threshold and
+                dist(ax2, ay2, bx1, by1) < threshold
+            )
         )
+
+    def cleanup(self, lines):
+        while True:
+            change = False
+
+            for i in range(len(lines)):
+                for j in range(len(lines)):
+                    if i == j: continue
+
+                    if self.lines_too_close(lines[i][0], lines[j][0], 15):
+                        lines = np.delete(lines, j, 0)
+                        change = True
+                        break
+
+                if change: break
+            if not change:
+                break
+
+        return lines
+
+    def detect(
+        self,
+        source,
+        hough_params,
+        canny_thresholds = (70, 60),
+        show_image = True,
+        save_image = False,
+    ):
+        out = source.copy()
+        gray = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray, *canny_thresholds)
+
+        lines = cv2.HoughLinesP(edges, *hough_params)
+        lines = self.cleanup(lines)
+
+        i = 0
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(out, (x1, y1), (x2, y2), colors[i % len(colors)], 2)
+
+        if show_image:
+            cv2.imshow('lines', out)
+            cv2.imshow('original', source)
+            cv2.waitKey(0)
+
+        if save_image:
+            cv2.imwrite(out(sys.argv[1], out))
+
+if __name__ == '__main__':
+    ld = LineDetector()
+    params = HoughParams(
+        rho=0.5,
+        theta=np.pi / 180,
+        threshold=20,
+        minLineLength=30,
+        maxLineGap=5
     )
+    source = cv2.imread(sys.argv[1])
 
-def cleanup(lines):
-    while True:
-        change = False
-
-        for i in range(len(lines)):
-            for j in range(len(lines)):
-                if i == j: continue
-
-                if lines_too_close(lines[i][0], lines[j][0], 15):
-                    lines = np.delete(lines, j, 0)
-                    change = True
-                    break
-
-            if change: break
-        if not change:
-            break
-
-    return lines
-
-img = cv2.imread(sys.argv[1])
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-edges = cv2.Canny(gray, 20, 20)
-
-cv2.imshow('edges', edges)
-cv2.waitKey(0)
-
-lines = cv2.HoughLinesP(
-    edges,
-    1,
-    np.pi / 100,
-    65,
-    minLineLength=15,
-    maxLineGap=250
-)
-
-# si hay dos líneas paralelas muy cerca, descarta una
-lines = cleanup(lines)
-
-i = 0
-for line in lines:
-    x1, y1, x2, y2 = line[0]
-    cv2.line(img, (x1, y1), (x2, y2), colors[i % len(colors)], 2)
-    i += 1
-
-cv2.imshow('lines', img)
-cv2.waitKey(0)
-
-# cv2.imwrite(deseado(sys.argv[1]), img)
+    ld.detect(source, params)
